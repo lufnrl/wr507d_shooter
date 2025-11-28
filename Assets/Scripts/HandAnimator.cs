f911367880f5d176ecfc057a4bb1d7017b27a2eb
developedbylucie <lfreihaut04@gmail.com>
2025-11-28 10:23:51 +0100

    feat(hand): add animation

---
+++ Assets/Scripts/HandAnimator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimator : MonoBehaviour
+{
+    public InputActionProperty triggerAction;
+    public InputActionProperty gripAction;
+    float _triggerValue;
+    float _gripValue;
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _triggerValue = triggerAction.action.ReadValue<float>();
+        _gripValue = gripAction.action.ReadValue<float>();
+
+        animator.SetFloat("Trigger", _triggerValue);
+        animator.SetFloat("Grip", _gripValue);
+    }
+}
