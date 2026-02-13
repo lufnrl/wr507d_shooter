59e444457f868228797bad3fd74821c22f2555b0
developedbylucie <lfreihaut04@gmail.com>
2026-02-13 23:38:12 +0100

    connect points to score rock

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     // Singleton : Allow to access the script anywhere with "ScoreManager.Instance"
     public static ScoreManager Instance;
-
+    public TextMeshProUGUI scoreText;
     private int currentScore = 0;
 
     private void Awake()
@@ -21,9 +22,27 @@ public class ScoreManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        // On initialise l'affichage à 0 au lancement du jeu
+        UpdateScoreDisplay();
+    }
+
     public void AddScore(int pointsToAdd)
     {
         currentScore += pointsToAdd;
         Debug.Log($"CIBLE TOUCHÉE ! (+{pointsToAdd}) | Score Total : {currentScore}");
+
+        // On met à jour la pierre à chaque point gagné !
+        UpdateScoreDisplay();
+    }
+
+    private void UpdateScoreDisplay()
+    {
+        // On vérifie que la case n'est pas vide pour éviter les erreurs
+        if (scoreText != null) 
+        {
+            scoreText.text = "SCORE : " + currentScore.ToString();
+        }
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingTarget : MonoBehaviour, IHittable
+public class Target : MonoBehaviour, IHittable
 {
-    [SerializeField]
-    private AudioSource audioSource;
-    [SerializeField] private int pointsValue = 10; // Valeur par défaut, modifiable dans l'inspecteur
+    [Header("Réglages")]
+    [SerializeField] private int pointsValue = 10; 
+    [SerializeField] private float timeBeforeDestroy = 0.15f;
 
-    // private void OnCollisionEnter(Collision collision)
-    // {
-    //     if ((rb.isKinematic || collision.gameObject.CompareTag("Arrow")) == false)
-    //     {
-    //         audioSource.Play();
-    //     }
-    // }
+    [Header("Effets")]
+    // On demande directement le fichier son, plus besoin du composant AudioSource !
+    [SerializeField] private AudioClip destructionSound; 
+    [SerializeField] [Range(0f, 1f)] private float soundVolume = 1.0f;
 
     public void GetHit()
     {
-        // 1. On prévient le Manager d'ajouter les points
+        // Say to manager to add points
         if (ScoreManager.Instance != null)
         {
             ScoreManager.Instance.AddScore(pointsValue);
         }
+
+        // Impact Sound
+        if (destructionSound != null)
+        {
+            AudioSource.PlayClipAtPoint(destructionSound, transform.position, soundVolume);
+        }
+
+        // Destroy ufo and sticky arrow child
+        // Debug.Log($"Target hit! +{pointsValue} points");
+        Destroy(gameObject, timeBeforeDestroy);
     }
 }
 
