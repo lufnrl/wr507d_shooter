b3864658e4f13b37bb188038e59bc2b8c5df4547
developedbylucie <lfreihaut04@gmail.com>
2026-02-20 22:30:38 +0100

    add explosion to ufo

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -10,10 +10,18 @@ public class Target : MonoBehaviour, IHittable
 
     [Header("Effets")]
     [SerializeField] private AudioClip destructionSound; 
-    [SerializeField] [Range(0f, 1f)] private float soundVolume = 1.0f;
+    [SerializeField] [Range(0f, 1f)] private float soundVolume = 1f;
+
+    [SerializeField] private GameObject explosionPrefab; 
+    [SerializeField] private float explosionScale = 0.5f;
+
+    private bool isAlreadyHit = false; // Security to prevent a 2nd arrow from triggering the explosion twice
 
     public void GetHit()
     {
+        if (isAlreadyHit) return;
+        isAlreadyHit = true;
+
         // Say to manager to add points
         if (ScoreManager.Instance != null)
         {
@@ -24,6 +32,24 @@ public class Target : MonoBehaviour, IHittable
         if (destructionSound != null)
         {
             AudioSource.PlayClipAtPoint(destructionSound, transform.position, soundVolume);
+        }
+
+        if (explosionPrefab != null)
+        {
+            // On fait apparaître l'explosion à la position exacte de la soucoupe
+            GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            
+            // On ajuste sa taille si besoin (pratique si tu utilises la même explosion que le boss mais en plus petit)
+            explosion.transform.localScale = new Vector3(explosionScale, explosionScale, explosionScale);
+            
+            // On détruit l'objet d'explosion après 2 secondes pour ne pas polluer la mémoire du jeu
+            Destroy(explosion, 2f);
+        }
+
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in renderers)
+        {
+            r.enabled = false;
         }
 
         // Destroy ufo and sticky arrow child
